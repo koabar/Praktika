8b5bd320b192327078a0bc86188e2424fc00ce6c
koabar <67659862+koabar@users.noreply.github.com>
2020-07-09 10:48:26 +0300

    Test

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+package main.java;
+
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome");
+        System.out.println("To");
+        System.out.println("GeeksforGeeks");
+    }
+}
